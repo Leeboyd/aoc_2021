7c82db40bf5109918feaa64ac5928faec517e641
LeeBoyd <jobboy19890101@gmail.com>
2021-12-13 18:27:28 +0800

    add day13

---
+++ day13.py
@@ -0,0 +1,44 @@
+import re
+
+def fold(axis, coordinate, points):
+    new_points = set()
+    for x, y in points:
+        if axis == 'y' and y > coordinate:
+            new_points.add((x, coordinate * 2 - y))
+        elif axis == 'x' and x > coordinate:
+            new_points.add((coordinate * 2 - x, y))
+        else:
+            new_points.add((x, y))
+    return new_points
+
+def print_grid(points):
+    max_x = max(x for x, _ in points) + 1
+    max_y = max(y for _, y in points) + 1
+    for y in range(max_y):
+        row = ('#' if (x, y) in points else ' ' for x in range(max_x))
+        print(' '.join(row))
+
+with open('input/day13.txt') as f:
+    points, folds = f.read().strip().split('\n\n')
+
+points = re.findall(r'(\d+),(\d+)', points)
+points = {(int(x), int(y)) for x, y in points}
+
+folds = re.findall(r'fold along ([xy])=(\d+)', folds)
+folds = [(axis, int(coordinate)) for axis, coordinate in folds]
+
+print(len(fold(*folds[0], points)))
+
+for axis, coordinate in folds:
+    points = fold(axis, coordinate, points)
+    
+print_grid(points)
+
+'''
+    # #   # # #     # # # #   # # #     #           # #     #     #   #     #
+      #   #     #         #   #     #   #         #     #   #   #     #     #
+      #   #     #       #     # # #     #         #         # #       # # # #
+      #   # # #       #       #     #   #         #   # #   #   #     #     #
+#     #   #   #     #         #     #   #         #     #   #   #     #     #
+  # #     #     #   # # # #   # # #     # # # #     # # #   #     #   #     #
+'''
