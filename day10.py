9fb1b962363324df5a9fccd9ae76d525c4baf612
LeeBoyd <jobboy19890101@gmail.com>
2021-12-17 21:43:39 +0800

    add day10 answer 2

--- day10.py
+++ day10.py
@@ -19,10 +19,13 @@ corrupted_score = {
     ">": 25137
 }
 
+incomplete_score = {")": 1, "]": 2, "}": 3, ">": 4}
+
 with open("input/day10.txt") as f:
     data = f.readlines()
 
 answer1 = 0
+answer2_candidates = []
 for line in data:
     stack = []
     for c in line.strip():
@@ -32,5 +35,12 @@ for line in data:
                 break
         else:
             stack.append(c)
+    else:
+        score = 0
+        for c in reversed(stack):
+           score = score * 5 + incomplete_score[match_parenthesis[c]]
+        answer2_candidates.append(score)
 
+answer2 = sorted(answer2_candidates)[len(answer2_candidates) // 2]
 print(answer1)
+print(answer2)
