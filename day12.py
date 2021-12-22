0dab02a08947ffff5c2cd81e579c6e68297ebe36
LeeBoyd <jobboy19890101@gmail.com>
2021-12-22 14:00:18 +0800

    add day12 answer2

--- day12.py
+++ day12.py
@@ -15,6 +15,18 @@ def dfs(start, seen):
     
     return s
 
+def dfs_2(start, seen, as_start=False):
+    if start == 'end':
+        return 1
+
+    s = 0
+    for end in d[start]:
+        if end not in seen:
+            tmp = {end} if end.islower() else set()
+            s += dfs_2(end, seen | tmp, as_start)
+        elif as_start and end != "start":
+            s += dfs_2(end, seen, False)
+    return s
 
 d = defaultdict(set)
 
@@ -23,5 +35,9 @@ for line in data:
     d[start].add(end)
     d[end].add(start)
 
+# small caves can be only visited once
 answer_1 = dfs("start", {"start"})
 print(answer_1)
+# small caves can be visited 2 times
+answer_2 = dfs_2("start", {"start"}, True)
+print(answer_2)
